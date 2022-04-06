10f6a57d2cd51afeea93c884d20a6126c48b15ef
LilRM1 <101335919+LilRM1@users.noreply.github.com>
2022-04-06 10:50:42 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,100 @@
+//import React from 'react';
+import React, {useState} from "react";
+import './App.css';
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+// eslint-disable-next-line
+import { useAuthState} from 'react-firebase-hooks/auth';
+import { useCollectionData} from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBDFQX8ZI5dzA2NAnXj752bcVaoRl6I_Kk",
+  authDomain: "mentor-matcher-g14.firebaseapp.com",
+  projectId: "mentor-matcher-g14",
+  storageBucket: "mentor-matcher-g14.appspot.com",
+  messagingSenderId: "236114227145",
+  appId: "1:236114227145:web:ef51ed9699906dbc2b8218",
+  measurementId: "G-BMFHK0P8JJ"
+})
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+function App() {
+
+  const [user] = useAuthState(auth);
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+      <section>
+        {user? <this.ChatRoom/>: <this.ChatRoom/> }
+      </section>
+    </div>
+  );
+}
+
+
+export function ChatRoom(){
+  
+  const messagesRef = firestore.collection('messaged');
+  const query = messagesRef.orderBy('createdAt').limit(25);
+
+  const[messages] = useCollectionData(query, {idField: 'id'});
+  const[formValue, setFromValue] = useState('');
+
+  const sendMessage = async(e) => {
+
+    e.preventDefault();
+
+   // const{uid, photoURL} = auth.currentUser;
+
+    await messagesRef.add({
+      text:formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+    })
+
+    setFromValue('');
+
+  }
+
+  return (
+    <>
+      <div>
+        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+       
+      </div>
+
+      <form onSubmit={sendMessage}>
+
+        <input value={formValue} onChange={(e) => setFromValue(e.target.value)}/>
+        <button type="submit"> Enter </button>
+      </form>
+    </>
+  )
+
+}
+
+function ChatMessage(props){
+  const{text,uid,photoURL} = props.message;
+
+  const messageClass = uid === auth.currentUser.uid? 'sent' : 'received';
+
+
+  return (
+    <div className = {`message ${messageClass}}`}>
+      <img src={photoURL} alt="user"/>
+      <p>{text}</p>
+    </div>
+  )
+}
+
+export default App;
