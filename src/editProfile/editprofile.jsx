3be16c4b1ae3b168091aa9ab389815d18f3ffb64
Asif2714 <asifabdullah47@gmail.com>
2022-04-09 15:21:04 +0100

    initial changes to editprofile

--- src/editProfile/editprofile.jsx
+++ src/editProfile/editprofile.jsx
@@ -2,7 +2,7 @@ import React,{useRef, useState} from "react";
 import {firestore} from  "../firebase/firebase-config";
 import {addDoc,collection } from "@firebase/firestore"
 import { storage } from "../firebase/firebase-config";
-import { signup, login, logout, useAuth } from "../firebase/firebase-config";
+import { signup, login, logout, useAuth , auth, db} from "../firebase/firebase-config";
 import Profile from "./Profile";
 
 
@@ -40,6 +40,22 @@ export default function Home(){
         }
 
     }
+
+    function GoalStrength(){
+      if(useAuth()){
+        if(auth.currentUser.email == "billy@jenkins.com"){
+          return (
+            <div className ="col-md-3"><p className="description">Goals:</p></div>
+          )
+        }
+        else{
+          return (
+            <div className ="col-md-3"><p className="description">Strengths:</p></div>
+          )
+        }
+      }
+      }
+      
 
     return(
 
@@ -94,13 +110,105 @@ export default function Home(){
                         <div className="col-md-9"><input type="text" className="form-control" ref ={PassRef} placeholder="Password"></input></div>
                       </div>
 
-                      <div className="row mt-4">
+                      {/* <div className="row mt-4">
                         <div className ="col-md-3"><p className="description">Preferences:</p></div>
                         <div className="col-md-9"><input type="text" className="form-control" ref ={preferencesRef} placeholder="Preferences"></input></div>
+                      </div> */}
+
+                        {/* NEED to make changes from below: */}
+                        {/* Have to change the Ref */}
+
+                      <div className="row mt-4">
+                        <div class="form-group">
+                        <label for="personalIntroduction">Personal Introduction:</label>
+                        <textarea class="form-control" id="personalIntroducton" rows="2"></textarea>
+                        </div>
+                      </div>
+                        
+                      <div className="row mt-4">
+                        <div className ="col-md-3"><p className="description">Location:</p></div>
+                        <div className="col-md-9"><input type="text" className="form-control" ref ={preferencesRef} placeholder="Location"></input></div>
                       </div>
 
+                      <div className="row mt-4">
+                        <div className ="col-md-3"><p className="description">Occupation:</p></div>
+                        <div className="col-md-9"><input type="text" className="form-control" ref ={preferencesRef} placeholder="Occupation"></input></div>
+                      </div>
 
+                      <div className="row mt-4">
+                      <div className ="col-md-3"><p className="description">Sector:</p></div>
+                        <select class="col-md-9 form-select form-select-sm" aria-label=".form-select-sm example">
+                          <option disabled selected>Open this select menu</option>
+                          <option value="technology">Technology</option>
+                          <option value="marketing">Marketing</option>
+                          <option value="marketing">Consulting</option>
+                          <option value="marketing">Research</option>
+                          <option value="other">Other</option>
+                        </select>
+                      </div>
 
+                      <div className="row mt-4">
+                      <div className ="col-md-3"><p className="description">Qualifications:</p></div>
+                        <select class="col-md-9 form-select form-select-sm" aria-label=".form-select-sm example">
+                          <option disabled selected>Open this select menu</option>
+                          <option value="1">BSc</option>
+                          <option value="2">MSc</option>
+                          <option value="3">PhD</option>
+                          <option value="3">Other</option>
+                        </select>
+                      </div>
+                          Upto qualifications, everything is common for Mentor & Mentee
+                          <br/>
+                          Entries for Mentees
+                      <div className="row mt-4">
+                        <GoalStrength/>
+                        <div className="col-md-9">
+                        <div class="form-check">
+                          <input class="form-check-input" type="checkbox" value="" id="presentationSkills" />
+                          <label class="form-check-label" for="presentationSkills">
+                            Presentation skills
+                          </label>
+                        </div>
+                        <div class="form-check">
+                          <input class="form-check-input" type="checkbox" value="" id="designThinking" />
+                          <label class="form-check-label" for="designThinking">
+                            Design Thinking
+                          </label>
+                        </div>
+                        <div class="form-check">
+                          <input class="form-check-input" type="checkbox" value="" id="leadershipSkills" />
+                          <label class="form-check-label" for="leadershipSkills">
+                            Leadership skills
+                          </label>
+                        </div>
+                        <div class="form-check">
+                          <input class="form-check-input" type="checkbox" value="" id="verbalCommunications" />
+                          <label class="form-check-label" for="verbalCommunications">
+                            Verbal Communications
+                          </label>
+                        </div>
+                        <div class="form-check">
+                          <input class="form-check-input" type="checkbox" value="" id="careerProgression" />
+                          <label class="form-check-label" for="careerProgression">
+                            Career Progression
+                          </label>
+                        </div>
+                        <div class="form-check">
+                          <input class="form-check-input" type="checkbox" value="" id="coding" />
+                          <label class="form-check-label" for="coding">
+                            Coding
+                          </label>
+                        </div>
+
+                        <div class="form-check">
+                          <input class="form-check-input" type="checkbox" value="" id="storytellingMentee" />
+                          <label class="form-check-label" for="storytellingMentee">
+                            Storytelling
+                          </label>
+                        </div>
+                        </div>
+                      </div>
+                      
                       <div className="mt-5 text-right"><button className="btn btn-primary profile-button " type="submit">Save Changes</button></div>
                   </div>
                 </div>
@@ -112,3 +220,11 @@ export default function Home(){
 
     )
 }
+
+// Mentee Profile:
+// -	
+// Mentor Profile:
+// -	Short introduction of themselves (could include awards and organisations)
+// -	Experience (in years)
+// -	LinkedIn profile link
+// -	What are your strengths?
