d9344c1f8cf92933caf020992b6d6f3096163803
Dil02 <77886292+Dil02@users.noreply.github.com>
2022-04-10 14:31:16 +0100

    View Mentee Requests nearly done without photos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import {useState, useEffect} from "react";
 import { FirebaseError } from 'firebase/app';
-import { storage } from "./firebase/firebase-config";
-import { ref, getBytes } from "firebase/storage";
 import {collection, getDoc, getDocs, addDoc, updateDoc, doc, deleteDoc, setDoc, Firestore, where, query} from 'firebase/firestore';
 import {db} from './firebase/firebase-config';
 import "./reset.css"; //Resets styling
-import "./View Mentees/viewMentees.css";
+import "./View Mentees/viewMenteeRequests.css";
+import { getAuth } from "firebase/auth";
 
 function App() {
 
@@ -17,35 +16,33 @@ function App() {
         const data = await getDocs(menteesCollectionRef);
         setMentees(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
     }
-    //Resume at the 15:48 mark pedro video for uploading videos.
     getMentees();
   }, []);
+  
 
   return (
     <div className="App">
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"/>
-      <div className="container fluid">
-        <h1>View Mentees</h1>
+      <div className="container-fluid">
+        <h1>View Mentee Requests</h1>
         {mentees.map((mentee) => {
           return(
-            <div class="col-sm pt-3">
-              <div class="row menteeProfiles p-3" >
-                <div class="col-m-2">
-                  <img class="profilePhoto" src=""></img>
+              <div className="row menteeProfiles p-3 mt-4">
+                <div className="col-md-2">
+                  <img className="profilePhoto" src=""></img>
                 </div>
-                <div class="col-lg-7 mt-3">
-                  <ul class="list-unstyled">
-                    <li class="pb-2"><h4>{mentee.firstName + " " + mentee.lastName}</h4></li>
+                <div className="col-lg-7">
+                  <ul className="list-unstyled">
+                    <li className="pb-2"><h4>{mentee.firstName + " " + mentee.surname}</h4></li>
                     <li><p>{mentee.emailAddress}</p></li>
                     <li><p>{mentee.phone}</p></li>
                   </ul>
                 </div>
-                <div class="col-m-2">
-                  <button type="button" class="btn btn-outline-success">Accept</button>
+                <div className="col col-lg-2">
+                  <button type="button" className="btn btn-outline-success">Accept</button>
                   <button type="button" class="btn btn-outline-danger">Decline</button>
                 </div>
               </div>
-            </div>
           );
         })}
         
